ac37202ade6eba5142d3056386d47a18d64e01bb
cantaruttim <cantaruttim@outlook.com>
2025-10-04 19:38:56 -0300

    bd files

---
+++ Banco de Dados/anotacoes.md
@@ -0,0 +1,13 @@
+# Banco de Dados
+1. Tipo de Bancos
+2. Ranking dos Bancos
+3. Sistemas e os Bancos de Dados
+
+## Aulas Especiais
+- Desafios
+    1. Jogo com JavaScript
+    2. Jogo da Cobrinha 
+        especificacoes
+            1. Use uma lista para poder criar a cobriha
+
+# Modelo Entidade Relacionamento
---
+++ JavaScript/variaveis.js
@@ -0,0 +1,41 @@
+// 1. QUAL A DIFERENÇA ENTRE VAR, LET E CONST?
+
+const PI = 3.14;
+console.log("Value of PI:", PI);
+
+let radius = 5;
+let area = PI * radius * radius;
+console.log("Area of circle with radius", radius, "is:", area);
+
+let nome = "Mathues"
+
+
+console.log("............");
+
+console.log(typeof(PI));
+console.log(typeof(radius));
+console.log(typeof(area));
+console.log(typeof(nome));
+
+// 2. SERÁ QUE PODEMOS MUDAR O TIPO DE UMA VARIÁVEL?
+let entrada = "42";
+console.log("Tipo do dado de entrada",typeof(entrada));
+let numero = Number(entrada);
+console.log("Número:", numero);
+console.log("Tipo do dado de saida",typeof(numero));
+
+
+
+const { Console } = require('console');
+// 3. COMO PODEMOS REALIZAR A ENTRADA DE DADOS?
+
+const readline = require('readline');
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+rl.question("Digite seu nome: ", function(nome) {
+  console.log("Olá, " + nome + "!");
+  rl.close();
+});

75df3c69938f27c27e831397335454fd3fb98594
Matheus de Almeida Cantarutti <81988636+cantaruttim@users.noreply.github.com>
2025-10-06 21:15:48 -0300

    Create exemplo.sql

---
+++ Banco de Dados/exemplo.sql
@@ -0,0 +1,12 @@
+CREATE TABLE pokemon (
+  cod_pokemon INT NOT NULL PRIMARY KEY,
+  nome_pokemon VARCHAR(50) NOT NULL,
+  class_pokemon VARCHAR(50) NOT NULL
+);
+
+INSERT INTO 
+	pokemon 
+    (cod_pokemon, nome_pokemon, class_pokemon) 
+    VALUES 
+    	(1, 'PICACHU', 'ELÉTRICO'),
+        (2, 'CHARMANDER', 'FOGO');
